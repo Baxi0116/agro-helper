ae35c7ca7b884d7e3607c235fb395b824343a327
Baxi0116 <gergely0116@freemail.hu>
2017-05-27 01:30:49 +0200

    OrchardOverview's final ui design finished

--- src/main/java/com/baxi/agrohelper/service/WorkServiceImpl.java
+++ src/main/java/com/baxi/agrohelper/service/WorkServiceImpl.java
@@ -1,9 +1,7 @@
 package com.baxi.agrohelper.service;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.baxi.agrohelper.dao.WorkDao;
